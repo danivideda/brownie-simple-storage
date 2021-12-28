66546f7add8646530cba576130c5853b457439bb
Dani Ihza Farrosi <dani.videda@gmail.com>
2021-12-28 13:13:24 +0700

    refactor accounts

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,16 @@
 from brownie import accounts
 import os
 
-def deploy_simple_storage():
-    accounts.add(os.getenv("PRIVATE_KEY"))
+def init_accounts():
+    account = accounts.add(os.getenv("PRIVATE_KEY"))
+    print("My Account: \n", account)
+
+    print("All account: ")
     for i in range(len(accounts)):
         print(accounts[i])
+
+def deploy_simple_storage():
+    init_accounts()
 
 def main():
     deploy_simple_storage()

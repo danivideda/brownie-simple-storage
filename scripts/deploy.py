d4fa2daf4f342ef8a172737a31843dcd26095db2
Dani Ihza Farrosi <dani.videda@gmail.com>
2021-12-29 08:42:00 +0700

    interracting with contract

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -10,14 +10,24 @@ def init_accounts():
     print("All account: ")
     for i in range(len(accounts)):
         print(accounts[i])
+    
+    print("==========")
 
 def deploy_simple_storage():
     init_accounts()
 
     print("Deploying contracts...")
     simple_storage = SimpleStorage.deploy({"from": account})
+    print("Contract deployed:", simple_storage)
 
-    print(simple_storage)
+    stored_value = simple_storage.retrieve()
+    print("Stored value:", stored_value)
+
+    print("Updating favorite number...")
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print("Updated Stored value:", updated_stored_value)
 
 def main():
     deploy_simple_storage()

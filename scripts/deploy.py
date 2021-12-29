cdccca134ab9ef4456e042dfe0771124a739ea3d
Dani Ihza Farrosi <dani.videda@gmail.com>
2021-12-29 09:35:06 +0700

    init_accounts for rinkeby, edited .env local file to include WEB3_INFURA_PROJECT_ID

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,18 +1,23 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 PRIVATE_KEY = config["wallets"]["private_key"]
 
 
 def init_accounts():
     global account
-    account = accounts.add(PRIVATE_KEY)
-    print("My Account: \n", account)
 
-    print("All account: ")
-    for i in range(len(accounts)):
-        print(accounts[i])
+    if network.show_active() == "development":
+        account = accounts[0]
 
-    print("==========")
+        print("My Account: \n", account)
+        print("All account: ")
+
+        for i in range(len(accounts)):
+            print(accounts[i])
+
+        print("==========")
+    else:
+        account = accounts.add(PRIVATE_KEY)
 
 
 def deploy_simple_storage():
